eed33cc252f5d79cdaa64435db7a0e76581220e5
Esakkidurai <112741698+esakki-durai@users.noreply.github.com>
2022-10-13 05:50:16 -0700

    Add files via upload

---
+++ ReverseNum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class ReverseNum{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+str=sc.nextLine();
+int sum=0,r;
+while(n>0){
+      r=n%10;
+       sum=sum*10+r;
+      n/=10;
+                }
+System.out.println("The Reversed number is :"+sum);
+                }
+}
