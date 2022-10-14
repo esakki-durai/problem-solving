f332423cdb9f85c371acd7defb704bb785e2f007
Esakkidurai <112741698+esakki-durai@users.noreply.github.com>
2022-10-13 19:19:43 -0700

    Add files via upload

---
+++ NumberPattern.java
@@ -0,0 +1,35 @@
+class NumberPattern{
+public static void main(String[] args){
+      	int [] []arr=new int[5] [5];
+                     int n=5;
+                     for(int i=0;i<n;i++){
+                         for(int j=0;j<n;j++){
+                                arr[i][j]=0;}}
+                                 
+int a=1;
+         int x=0;
+          int y=n-1;
+while(a<=n){
+              int val=n-1;
+           arr[x][y]=a;
+            int t=y;
+                               while(y<n-1){
+                                     int b=val+arr[x][y];
+	                y++;
+                                      arr[x][y]=b;
+                                       val--;}
+y=t;
+x++;
+y--;
+a++;
+}
+ for(int i=0;i<n;i++){
+                         for(int j=0;j<n;j++){
+                               
+                                      System.out.print(arr[i][j]+"         ");}
+                               System.out.println();}
+                                    
+ 
+ 
+               }
+        }
