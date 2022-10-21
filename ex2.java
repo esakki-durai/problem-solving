f79bc4b5e4e10c81d440f40f1a71df72c267a45e
Esakkidurai <112741698+esakki-durai@users.noreply.github.com>
2022-10-21 11:09:41 +0530

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -10,7 +10,6 @@ class ex2 {
         char[]  chars=str.toCharArray();//string is immutable so we change string into array.
         for(char c:chars){
            System.out.print(c);}
-
 int j= chars.length-1;
 int i=0;
 while(i<=j){
@@ -18,12 +17,8 @@ while(i<=j){
                      if((chars[i] >= 'a' && chars[i] <= 'z')||(chars[i] >= 'A' && chars[i]<= 'Z')){break;}
                      else{ i++;}}
              while(i<j){
-                    if((chars[j] >= 'a' && chars[j] <= 'z')||(chars[j] >= 'A' && chars[j] <= 'Z')){
-                           break;}
-                    else{
-                         j--;}
-                            }
-
+                    if((chars[j] >= 'a' && chars[j] <= 'z')||(chars[j] >= 'A' && chars[j] <= 'Z')){break;}
+                    else{ j--;} }
 char temp=chars[i];
 chars[i]=chars[j];
 chars[j]=temp;
@@ -32,6 +27,4 @@ j--;
 }
 System.out.println("After   string");
 for(i=0;i<chars.length-1;i++){
-   System.out.print(chars[i]);}
-    }
-       }
+   System.out.print(chars[i]);}}}
