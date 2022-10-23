f51960186519237bbcc3fb900695d8136572e52e
Esakkidurai <112741698+esakki-durai@users.noreply.github.com>
2022-10-23 13:41:22 +0530

    Add files via upload

---
+++ Find the mode of an array using java/ModeOfArray.java
@@ -0,0 +1,13 @@
+//Find the modenumber of an array.modenumber means most repeated value
+class ModeOfArray{
+public static void main(String[] args) {
+	int n=5,a[]={2,4,3,1,3},count,topvalue=0,topcount=0;
+	for(int i=0;i<n;i++){
+		count=0;
+		for(int j=i+1;j<n;j++){
+			if(a[i]==a[j]){
+				count++;}
+			if(topcount<count){
+				topcount=count;
+				topvalue=a[j];}}} 
+    System.out.println("The mode of array is:   "+topvalue);}}
