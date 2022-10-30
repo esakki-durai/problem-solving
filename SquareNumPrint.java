829798de6540f52c10c36706765dc401ab984991
Esakkidurai <112741698+esakki-durai@users.noreply.github.com>
2022-10-30 02:09:44 -0700

    Update SquareNumPrint.java

--- SquareNumPrint.java
+++ SquareNumPrint.java
@@ -10,27 +10,19 @@ output:
 4  3  3  3  3  3  4
 4  4  4  4  4  4  4
 */
+import java.util.Scanner;
 class SquareNumPrint{
 public static void main(String[] args){
-int [] []arr=new int[8] [8];
-int n=4;
-int m=(n*2)-1;
-int first=0;
-int last=6;
-int x=0;
-int y=6;
-while(first<=last){
-for(int i=x;i<=y;i++){
-   for(int j=x;j<=y;j++){
-      if((i==first)||(j==first)||(i==last)||(j==last)){
-            arr[i][j]=n;}}}
-x++;
-y--;
-n--;
-first++;
-last--;
-}
-for(int i=0;i<m;i++){
-  for(int j=0;j<m;j++){
-     System.out.print(arr[i][j]+"  ");}
-     System.out.println();}}}
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n=sc.nextInt();
+      	int [] []arr=new int[n*2] [n*2];
+                     int m=(n*2)-1,first=0,last=m-1,x=0,y=m-1;
+                     while(first<=last){
+                        for(int i=x;i<=y;i++){for(int j=x;j<=y;j++){
+                            if((i==first)||(j==first)||(i==last)||(j==last)){
+                                arr[i][j]=n;}}}
+                                x++;y--;n--;first++;last--;}
+                    System.out.println("The Square Printing is:");
+                    for(int i=0;i<m;i++){for(int j=0;j<m;j++){
+                                System.out.print(arr[i][j]+"  ");}System.out.println();}}}
